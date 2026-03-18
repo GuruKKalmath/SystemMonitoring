bbb369ea0597c5ff5a17c83562a931b6f6568e1d
Guru Kalmath <gurukkalmath@gmail.com>
2026-03-18 20:09:15 +0530

    First commit of the project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Model.MetricsData;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        MetricsData metricsData = new MetricsData();
+    }
+}
